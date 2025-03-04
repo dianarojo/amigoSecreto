6b9da768c81d09df8b5a004a2f5e122e944e3215
Diana Rojo <licdianarojo@gmail.com>
2025-03-03 22:40:04 -0600

    Lógica en JS para agregar los amigos ingresados y sortear al amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creamos el primer arrego  para guardar los amigos
+let amigos = [];
+ 
+//creamos la funcion para agregar amigos
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let amigosAgregados = inputAmigo.value.trim();
+    
+    if (amigosAgregados === "") {
+        alert("Por favor, ingresa un nombre");
+    } else {
+        amigos.push(amigosAgregados);
+        actualizarLista();
+        inputAmigo.value = ""; 
+    }
+}
+
+//funcion para actualizar la lista de amigos con cada ingreso del usuario
+function actualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; 
+    
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+//funcion para sortear el amigo secreto
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Debe haber al menos un amigo para realizar el sorteo");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    
+    let li = document.createElement("li");
+    li.textContent = `El amigo secreto es: ${amigoSorteado}`;
+    resultado.appendChild(li);
+
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    amigos = [];
+}
